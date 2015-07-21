91206532e29cd3d0f7a8fb138547d5d713be2040
Sandra Tirado <Sondi@Sandras-MacBook-Pro.local>
2015-07-20 19:39:31 -0500

    Postits are draggable and can be created where the mouse is positioned

--- post-it.js
+++ post-it.js
@@ -13,10 +13,27 @@ var Board = function( selector ) {
 };
 
 var PostIt = function() {
+  var self = this;
+  function initialize(){
+      self.element = $('.post-it').first()
+    };
+    initialize();
   // Aquí va el código relacionado con un post-it
 };
+
+// PostIt.prototype.close
 
 $(function() {
   // Esta es la fucnión que correrá cuando este listo el DOM 
   new Board('#board');
+  $('#board').dblclick(function(x){
+  console.log("click");
+  post_it = new PostIt();
+  (post_it.element).clone().appendTo($('#board')).css({
+    'left' : x.pageX,'top'  : x.pageY
+  });
+  $('.post-it').draggable({
+     handle: '.header'
+  });
+  });
 });
