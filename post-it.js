5593396a6539efc9dcbd5f72edd7bc5a1e0ca78e
Sandra Tirado <Sondi@Sandras-MacBook-Pro.local>
2015-07-21 10:55:14 -0500

    refactorize and postits can close now

--- post-it.js
+++ post-it.js
@@ -7,37 +7,48 @@ var Board = function( selector ) {
   
   function initialize() {
     // Que debe de pasar cuando se crea un nuevo tablero?
+      $elem.dblclick(function(event){
+      post_it = new PostIt(event.pageX, event.pageY);
+      $elem.append(post_it.$elem);
+    });
+
+      $elem.on('click', '.close', function(){
+      $(this).closest('.post-it').remove();
+    });
   };
 
   initialize();
 };
 
-var PostIt = function() {
+var PostIt = function(x,y) {
   var self = this;
   function initialize(){
-      self.element = $('.post-it').first()
-    };
-    initialize();
+    self.$elem = $('.post-it').first().clone();
+    self.style(x,y);
+    self.draggable();
+  };
+  initialize();
   // Aquí va el código relacionado con un post-it
+};
+
+PostIt.prototype.style = function(x,y) {
+  this.$elem.css({
+      'left' : x,'top'  : y, "display": "block"
+    });
+};
+
+PostIt.prototype.draggable = function() {
+  this.$elem.draggable({
+      handle: '.header'
+  });
 };
 
 
 $(function() {
   // Esta es la fucnión que correrá cuando este listo el DOM 
   new Board('#board');
-  $('.post-it').draggable();
-  $('#board').dblclick(function(x){
-  console.log("click");
-  post_it = new PostIt();
-  (post_it.element).clone().appendTo($('#board')).css({
-    'left' : x.pageX,'top'  : x.pageY
-  });
-  $('#board').on('click', '.close', function(){
-    console.log(this);
-    // $('.header').parent().remove();
-  });
-    $('.post-it').draggable({
-     handle: '.header'
-    });
-  });
+
 });
+
+
+
